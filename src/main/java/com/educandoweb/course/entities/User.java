dcd2c4e1cceb444f53bd0f03de264c9ccf18a150
Edurequia <edurequia@gmail.com>
2023-07-19 15:03:46 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,9 +3,20 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity // // indica que User é uma entidade que será mapeada para uma tabela em um banco de dados relacional.
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // especifica a estratégia de geração de valor para a coluna de chave primária id
 	private Long id;
 	private String name;
 	private String email;
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
 
-@RestController
+@RestController // indica que UserResource é um controlador REST, que lida com as requisições HTTP e retorna as respostas adequadas 
 @RequestMapping(value = "/users")
 public class UserResource {
 	
-	@GetMapping
+	@GetMapping // mapear uma rota HTTP GET para o método findAll()
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "Maria", "maria@gmail.com", "9999999999", "12345");
 		return ResponseEntity.ok().body(u);
