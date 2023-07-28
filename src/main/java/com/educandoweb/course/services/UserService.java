adc57a0c6b9e07810cf46b8869130e4438346ba0
Edurequia <edurequia@gmail.com>
2023-07-28 12:12:58 -0300

    ExceptionHandler findById

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service // registra a classe como um componente do spring, 
 // dai ele vai poder ser injetado automaticamente com o @Autowired no UserService
@@ -22,8 +23,8 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id); //optional é implementado pois o repository.findById pode 
-		return obj.get();			   // retornar um valor nulo, dai o obj.get retorna o valor q ta contido
-	}									// no optional, que pode ser o id ou uma exceção por estar vazio
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); // retornar um valor nulo, dai o obj.get retorna o valor q ta contido
+	}												// no optional, que pode ser o id ou uma exceção por estar vazio
 	
 	public User insert(User obj) {
 		return repository.save(obj);
